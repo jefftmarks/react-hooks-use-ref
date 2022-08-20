7e189d3adac2373fd1fa5af10f6911fe486d6276
Jeff Marks <jeff.t.marks@gmail.com>
2022-08-20 10:09:10 -0400

    done with assignment

--- src/components/CounterState.js
+++ src/components/CounterState.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function CounterState() {
   const [count, setCount] = useState(0);
@@ -7,10 +7,18 @@ function CounterState() {
     setCount((count) => count + 1);
   }
 
+  let refCount = useRef(0);
+
+  function handleRefClick() {
+    refCount.current += 1;
+  }
+
   return (
     <div>
       <h1>CounterState</h1>
       <button onClick={handleClick}>{count}</button>
+      <h1>CounterRef</h1>
+      <button onClick={handleRefClick}>{refCount.current}</button>
     </div>
   );
 }
